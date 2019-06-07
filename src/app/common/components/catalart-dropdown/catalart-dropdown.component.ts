72dc5761b311eb15dff2274f8e5b2b315d2e4a61
pastorsj <sampastoriza@yahoo.com>
2019-06-06 21:39:53 -0400

    Fix dropdown component

--- src/app/common/components/catalart-dropdown/catalart-dropdown.component.ts
+++ src/app/common/components/catalart-dropdown/catalart-dropdown.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl
+} from '@angular/forms';
 import { Enumeration } from '../../models/enumeration.model';
 
 @Component({
@@ -9,24 +16,43 @@ import { Enumeration } from '../../models/enumeration.model';
   providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: CatalartDropdownComponent, multi: true }]
 })
 export class CatalartDropdownComponent implements ControlValueAccessor {
-  catalogOptions: Enumeration[] = [];
+  @Input() options: Enumeration[] = [];
+  @Input() placeholderText: string;
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      selectedOption: [{}, [Validators.required]]
+    });
+  }
 
   onChange: any = () => {};
   onTouch: any = () => {};
 
-  set value(catalogOptions: Enumeration[]) {
-    this.catalogOptions = catalogOptions;
-    this.onChange(catalogOptions);
-    this.onTouch(catalogOptions);
+  set value(option: Enumeration) {
+    this.selectedOption.patchValue(option);
+    this.onChange(option);
+    this.onTouch(option);
   }
 
-  writeValue(value: Enumeration[]) {
+  writeValue(value: Enumeration) {
     this.value = value;
   }
+
   registerOnChange(fn: any) {
     this.onChange = fn;
   }
+
   registerOnTouched(fn: any) {
     this.onTouch = fn;
+  }
+
+  compareFn(e1: Enumeration, e2: Enumeration): boolean {
+    return e1 && e2 && e1.id === e2.id;
+  }
+
+  get selectedOption() {
+    return this.form.get('selectedOption');
   }
 }
